59097cc7ac21853791cdad565067110297fb73f9
dubrovinach <60196551+dubrovinach@users.noreply.github.com>
2020-05-17 21:08:34 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,13 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var uglify = require("gulp-uglify");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -23,6 +30,12 @@ gulp.task("css", function () {
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("uglify", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"))
 });
 
 gulp.task("server", function () {
@@ -44,16 +57,12 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-var gulp = require("gulp");
-var webp = require("gulp-webp");
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({ quality: 90 }))
     .pipe(gulp.dest("source/img"));
 });
 
-var gulp = require("gulp");
-var imagemin = require("gulp-imagemin");
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -64,22 +73,15 @@ gulp.task("images", function () {
     .pipe(gulp.dest("source/img"));
 });
 
-var gulp = require("gulp");
-var rename = require("gulp-rename");
-var svgstore = require("gulp-svgstore");
-
 gulp.task("sprite", function () {
   return gulp.src("source/img/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
     .pipe(gulp.dest("build/img"));
 });
-
-var gulp = require("gulp");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
@@ -89,7 +91,6 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
-var gulp = require("gulp");
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
@@ -102,17 +103,15 @@ gulp.task("copy", function () {
     .pipe(gulp.dest("build"));
 });
 
-var gulp = require("gulp");
-var del = require("del");
 gulp.task("clean", function () {
   return del("build");
 });
 
-var gulp = require("gulp");
 gulp.task("build", gulp.series(
   "clean",
   "copy",
   "css",
+  "uglify",
   "sprite",
   "html"
 ));
